a9ac703439ee317a5b012ad329216129848d0d24
Denys1994q <drybachok@gmail.com>
2023-10-16 13:21:21 +0300

    fix imgs sizes1

--- src/app/product/pages/product-video/product-video.component.ts
+++ src/app/product/pages/product-video/product-video.component.ts
@@ -10,16 +10,4 @@ export class ProductVideoComponent {
 
   constructor(public ProductService: ProductService) {}
 
-  // videoSlides: Slide[] = [
-  //   {video_url: 'https://video.rozetka.com.ua/video3/smartfony_tv_i_elektronika/aksessuary_dlya_telefonov/naushniki/naushniki_defunc_true_music_tws.mp4'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/zpbbo__IdEA', video_screen: '../../../assets/video_screen1.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/T2LKjrFpKfc', video_screen: '../../../assets/video_screen2.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/bZROsiW0_T8', video_screen: '../../../assets/video_screen3.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/zpbbo__IdEA', video_screen: '../../../assets/video_screen4.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/zpbbo__IdEA', video_screen: '../../../assets/video_screen1.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/T2LKjrFpKfc', video_screen: '../../../assets/video_screen2.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/bZROsiW0_T8', video_screen: '../../../assets/video_screen3.jpg'},
-  //   {video_youtubeUrl: 'https://www.youtube.com/embed/zpbbo__IdEA', video_screen: '../../../assets/video_screen4.jpg'},
-  // ]
-
 }
