cc00d7121b13fc3a83de85a0f26bdc9929a46e93
Denys1994q <drybachok@gmail.com>
2023-11-18 10:06:52 +0200

    before deploy9

--- src/app/core/pages/home/home.component.ts
+++ src/app/core/pages/home/home.component.ts
@@ -62,7 +62,7 @@ export class HomeComponent implements AfterViewInit {
     ngOnInit() {
         if (!this.productService.allCategories) {
             console.log('getAllCategories')
-            // this.productService.getAllCategories()
+            this.productService.getAllCategories()
         }
         this.cartService.getCart()
     }
